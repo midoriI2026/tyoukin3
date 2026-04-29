3439b5d40e0600e4bb0796be5b1b03c8e17b9dc9
midoriI2026 <106957602+midoriI2026@users.noreply.github.com>
2026-04-29 16:23:58 +0900

    Add user script for record comparison

---
+++ tyoukin3.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         ★超勤：今 vs 前レコード（確定版）
+// @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?page=DBTaskForm&did=150*
+// @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?Page=DBSearchResult&DID=150*
+// @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?page=DBRecord&did=150*
+// @run-at       document-end
+// @require      https://raw.githubusercontent.com/midoriI2026/tyoukin3/main/tyoukin3.js?v=1.0
+// @grant        none
+// ==/UserScript==
